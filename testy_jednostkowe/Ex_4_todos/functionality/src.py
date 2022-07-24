4812f2bbf0ff6264ebe38767ce659042f829268d
memorisanka <j.krawczyk87@gmail.com>
2022-07-24 12:48:42 +0200

    corrected test_todos and names of some files

--- testy_jednostkowe/Ex_4_todos/functionality/src.py
+++ testy_jednostkowe/Ex_4_todos/functionality/src.py
@@ -1,27 +1,27 @@
 todos = ["Clean my room", "Make my bed", "Go to school", "Do school homework"]
 
 
-def check_pos(pos):
+def check_pos(pos: int) -> None:
     if len(todos) == 0:
         raise Exception("No more todos!")
     elif pos >= len(todos) or pos < 0:
         raise Exception("No such item number!")
 
 
-def add_todo(content):
-    todos.append(content)
+def add_todo(todo: str) -> None:
+    todos.append(todo)
 
 
-def remove_todo(pos):
+def remove_todo(pos: int) -> None:
     check_pos(pos)
 
     todos.pop(pos)
 
 
-def edit_todo(pos, content):
+def edit_todo(pos: int, todo: str) -> None:
     check_pos(pos)
 
-    todos[pos] = content
+    todos[pos] = todo
 
 
 def remove_all():
@@ -32,4 +32,3 @@ add_todo("Go to bed")
 remove_todo(0)
 edit_todo(0, "Get up from bed")
 remove_all()
-
---
+++ testy_jednostkowe/Ex_4_todos/tests/test_todos.py
@@ -0,0 +1,59 @@
+from functionality.src import *
+import pytest
+
+
+def create_test_todos():
+    laundry = "Make laundry"
+    bed = "Make bed"
+    dinner = "Make dinner"
+    add_todo(laundry)
+    add_todo(dinner)
+    add_todo(bed)
+
+
+def test_if_check_pos_throws_No_such_item_number_exception():
+    with pytest.raises(Exception) as ex1:
+        create_test_todos()
+        check_pos(20)
+    assert "No such item number!" in str(ex1.value)
+
+
+def test_if_check_pos_throws_No_more_todos_exception():
+    with pytest.raises(Exception) as ex2:
+        remove_all()
+        check_pos(1)
+    assert "No more todos!" in str(ex2.value)
+
+
+def test_check_if_adds_todo():
+    todo = "Make laundry"
+    add_todo(todo)
+    expected = todos[len(todos) - 1]
+
+    assert expected == todo
+
+
+def test_check_if_removes_todo():
+    # todos jest puste
+    create_test_todos()
+    length_before = len(todos)
+
+    pos = 1
+    remove_todo(pos)
+    length_after = len(todos)
+
+    assert length_after == length_before - 1
+
+
+def test_check_if_edits_todo():
+    create_test_todos()
+    pos = 1
+    todo = "Make dinner"
+    edit_todo(pos, todo)
+
+    assert todos[pos] == todo
+
+
+def test_check_if_removes_all():
+    todos.clear()
+    assert todos == []  # or len(todos) == 0

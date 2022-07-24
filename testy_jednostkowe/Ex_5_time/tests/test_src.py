4a11cfb16563b091cbcc8830782d886e9d232dd8
memorisanka <j.krawczyk87@gmail.com>
2022-07-24 22:17:21 +0200

    test_calc_time_diff

---
+++ testy_jednostkowe/Ex_5_time/tests/test_src.py
@@ -0,0 +1,14 @@
+from functionality.src import *
+from freezegun import freeze_time
+
+
+
+@freeze_time("2021-11-04T09:22:28+00:00")
+def test_should_return_time_diff_in_seconds():
+    expected = 86400.0
+    case = {
+        'start_time': '2021-11-03T09:22:28+00:00',
+        'end_time': None
+    }
+
+    assert calc_diff(case) == expected
